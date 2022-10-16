ff944fbdfb13994b1d2d86d48cc3c958f792496b
Filip Iran <iranf.03@spst.cz>
2022-10-16 16:28:20 +0200

    Přidejte soubory projektu.

---
+++ multiform_ukol/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace multiform_ukol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 134);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Zadat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(70, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Vymazat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(143, 184);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Multiform Úkol1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ multiform_ukol/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multiform_ukol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int pocet = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pocet < 10)
+            {
+                Button button1 = new Button();
+                button1.DialogResult = DialogResult.OK;
+                button1.Location = new Point(5, 30);
+                button1.Size = new Size(50, 25);
+                button1.Text = "Ulož";
+
+                Button button2 = new Button();
+                button2.DialogResult = DialogResult.Cancel;
+                button2.Location = new Point(95, 30);
+                button2.Size = new Size(50, 25);
+                button2.Text = "Zrušit";
+
+                TextBox textBox = new TextBox();
+                textBox.Location = new Point(5, 5);
+                textBox.Size = new Size(140, 50);
+                textBox.Clear();
+
+                Form form2 = new Form();
+                form2.Location = new Point(0, 1000);
+                form2.Size = new Size(165, 100);
+                form2.MaximizeBox = false;
+                form2.MinimizeBox = false;
+                form2.FormBorderStyle = FormBorderStyle.FixedDialog;
+                form2.Controls.Add(textBox);
+                form2.Controls.Add(button2);
+                form2.Controls.Add(button1);
+                form2.Text = "Zadávání";
+
+                form2.AcceptButton = button1;
+                form2.ActiveControl = textBox;
+
+                if (form2.ShowDialog() == DialogResult.OK)
+                {
+                    listBox1.Items.Add(textBox.Text);
+                    pocet++;
+                }
+            }
+            else
+            {
+                MessageBox.Show("V listBoxu může být jen 10 položek!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            pocet = 0;
+        }
+    }
+}
